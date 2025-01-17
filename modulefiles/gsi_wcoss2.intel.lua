f721caa1d3c884beb728a11c98dde8c8def8650b
David Huber <david.huber@noaa.gov>
2025-01-17 17:15:16 +0000

    Unset compiler flags

--- modulefiles/gsi_wcoss2.intel.lua
+++ modulefiles/gsi_wcoss2.intel.lua
@@ -20,6 +20,18 @@ load(pathJoin("cmake", cmake_ver))
 
 pushenv("CRTM_FIX", "/apps/ops/prod/libs/intel/19.1.3.304/crtm/2.4.0.1/fix")
 
+local CFLAGS=os.getenv("CFLAGS") or ""
+local CXXFLAGS=os.getenv("CXXFLAGS") or ""
+local FFLAGS=os.getenv("FFLAGS") or ""
+
+CFLAGS=CFLAGS:gsub("-static-libgcc -static-libstdc++ -Bstatic -lstdc++ -Bdynamic -lm -lpthread","")
+CXXFLAGS=CXXFLAGS:gsub("-static-libgcc -static-libstdc++ -Bstatic -lstdc++ -Bdynamic -lm -lpthread","")
+FFLAGS=FFLAGS:gsub("-static-libgcc -static-libstdc++ -Bstatic -lstdc++ -Bdynamic -lm -lpthread","")
+
+unsetenv("CFLAGS")
+unsetenv("CXXFLAGS")
+unsetenv("FFLAGS")
+
 pushenv("GSI_BINARY_SOURCE_DIR", "/lfs/h2/emc/global/noscrub/emc.global/FIX/fix/gsi/20241022")
 
 whatis("Description: GSI environment on WCOSS2")
